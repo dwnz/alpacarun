8686a6faf7d91cbf426f1a3358b59ab2eff51159
Daniel Wylie <daniel@danielwylie.me>
2017-06-16 10:19:23 +1200

    Simplify

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-mkdir _deploy
-
-cp *.html _deploy/
-cp -r img _deploy/
-cp -r js _deploy/
-cp -r sound _deploy/
 
 scp -P 63622 -r . shnapper@shnappy.com:/var/apps/alpacarun.danielwylie.me/
 scp -P 51316 -r . shnapper@shnappy.com:/var/apps/alpacarun.danielwylie.me/
-
-rm -r _deploy
