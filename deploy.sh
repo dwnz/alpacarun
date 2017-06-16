eaa5c7cce4f3a7faa3a17630af2cf97f85f8b922
Daniel Wylie <daniel@danielwylie.me>
2017-06-16 12:25:57 +1200

    Deploy fix

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ cp -r img _deploy/
 cp -r js _deploy/
 cp -r sound _deploy/
 cp *.html _deploy/
+cp *.txt _deploy/
 
 scp -P 63622 -r _deploy/ shnapper@shnappy.com:/var/apps/alpacarun.danielwylie.me/
 scp -P 51316 -r _deploy/ shnapper@shnappy.com:/var/apps/alpacarun.danielwylie.me/
