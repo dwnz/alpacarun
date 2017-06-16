f01165e8b0f146f0c8f4f09eb87b442157eca01d
Daniel Wylie <daniel@danielwylie.me>
2017-06-16 12:17:38 +1200

    Fast deploy + offline support

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+mkdir _deploy
 
-scp -P 63622 -r . shnapper@shnappy.com:/var/apps/alpacarun.danielwylie.me/
-scp -P 51316 -r . shnapper@shnappy.com:/var/apps/alpacarun.danielwylie.me/
+cp -r css _deploy/
+cp -r img _deploy/
+cp -r js _deploy/
+cp -r sound _deploy/
+cp *.html _deploy/
+
+scp -P 63622 -r _deploy/ shnapper@shnappy.com:/var/apps/alpacarun.danielwylie.me/
+scp -P 51316 -r _deploy/ shnapper@shnappy.com:/var/apps/alpacarun.danielwylie.me/
+
+rm -r _deploy
